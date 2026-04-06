7232606068ff0fb11502eb07422d7cba1603e736
Aplaking <aplaking@carc.pshs.edu.ph>
2026-04-06 09:34:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,28 @@ try:
         # Load the JSON data from the file
         data = json.load(file)
 
+    # ANSI codes (DECORATIVE TEXT FORMATS)
+	black = "\033[0;30m"
+	red = "\033[0;31m"
+    green = "\033[0;32m"
+    brown = "\033[0;33m"
+    blue = "\033[0;34m"
+    purple = "\033[0;35m"
+    cyan = "\033[0;36m"
+    light_gray = "\033[0;37m"
+    dark_gray = "\033[1;30m"
+    light_red = "\033[1;31m"
+    light_green = "\033[1;32m"
+    yellow = "\033[1;33m"
+    light_blue = "\033[1;34m"
+    light_purple = "\033[1;35m"
+    light_cyan = "\033[1;36m"
+    light_white = "\033[1;37m"
+    bold = "\033[1m"
+    faint = "\033[2m"
+    italic = "\033[3m"
+	reset = "\033[0m"
+
     # Interface for project Mirari
     print("Welcome to our game")
 
